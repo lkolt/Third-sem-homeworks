1b5f81f9f2da7d199be9bbefb3c7baf07e932c57
Kanteev Leonid <lkolt2@mail.ru>
2016-11-27 23:43:48 +0400

    Update HttpSniffer.cpp
    fix

--- SimpleHttpSniffer.cpp
+++ SimpleHttpSniffer.cpp
@@ -10,8 +10,6 @@
 #include <linux/if_ether.h>
 #include <linux/if_arp.h>
 
-#include<netinet/ip_icmp.h>   //Provides declarations for icmp header
-#include<netinet/udp.h>   //Provides declarations for udp header
 #include<netinet/tcp.h>   //Provides declarations for tcp header
 #include<netinet/ip.h>    //Provides declarations for ip header
 
@@ -22,6 +20,7 @@
 	} \
 } while(0)
 
+FILE *file;
 #define BUF_SZ 2048
 
 char in_buf[BUF_SZ];
@@ -29,14 +28,15 @@ char in_buf[BUF_SZ];
 void hex_print(char *buf, int len) {
 	const int width = 16;
 	for (int i = 0; i < len; ++i) {
-		printf("%02hhx%c", buf[i], ((i+1) % width == 0) ? '\n' : ' ');
+		fprintf(file, "%c%c", buf[i], ((i+1) % width == 0) ? '\n' : ' ');
 	}
 
 	if ((len % width) != 0) {
-		putchar('\n');
-	}
-	putchar('\n');
+        fprintf(file, "\n");
+    }
+    fprintf(file, "\n");
 }
+
 
 void ip_print(char *buf){
     unsigned short iphdrlen;
@@ -51,18 +51,20 @@ void ip_print(char *buf){
 
     memset(&dest, 0, sizeof(dest));
     dest.sin_addr.s_addr = iph->daddr;
-    printf("Source IP     : %s\n", inet_ntoa(source.sin_addr));
-    printf("Destination IP: %s\n", inet_ntoa(dest.sin_addr));
+    fprintf(file, "Source IP     : %s\n", inet_ntoa(source.sin_addr));
+    fprintf(file, "Destination IP: %s\n", inet_ntoa(dest.sin_addr));
 }
 
 int main(int argc, char *argv[]) {
-	int raw_sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	file = fopen("log2.txt", "w");
+
+    	int raw_sock = socket(AF_INET , SOCK_RAW , IPPROTO_TCP);
 	exit_on_error(raw_sock);
 
 	int read_cnt;
 	while (0 < (read_cnt = read(raw_sock, in_buf, BUF_SZ))) {
 
-        printf("\n\n\***********************TCP Packet*************************\n\n");
+        fprintf(file, "\n\n\***********************TCP Packet*************************\n\n");
         unsigned short iphdrlen;
         struct iphdr *iph = (struct iphdr *)in_buf;
         iphdrlen = iph->ihl * 4;
@@ -70,9 +72,10 @@ int main(int argc, char *argv[]) {
         struct tcphdr *tcph = (struct tcphdr*)(in_buf + iphdrlen);
 
         ip_print(in_buf);
-        printf("Data          :\n");
+        fprintf(file, "Data          :\n");
         int tcphlen = (tcph->doff) * 4;
         int iphlen = (iph->ihl) * 4;
         hex_print(in_buf + iphdrlen + tcphlen, read_cnt - tcphlen - iphlen);
 	}
 }
+
